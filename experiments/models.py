c5d850680f87c1564aa2b212ddfe6d7b74826161
Giorgos Kordopatis-Zilos <georgekordopatis@iti.gr>
2022-03-11 21:14:52 +0200

    Fix last layer initialization

--- experiments/models.py
+++ experiments/models.py
@@ -1,6 +1,9 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
+
 import timm
+from timm.models.layers import trunc_normal_
 
 """
 This module contains the models that are used for experimentation.
@@ -8,13 +11,11 @@ Models are currently used from timm.
 """
 
 
-#ResNet50:
-class resnet50(nn.Module):
+class BaseDetector(nn.Module):
 
     def __init__(self):
-        super(resnet50, self).__init__()
-        self.model = timm.create_model('resnet50', pretrained=True)
-        self.model.fc = nn.Linear(self.model.fc.in_features, 1)
+        super(BaseDetector, self).__init__()
+        self.model = None
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
@@ -26,10 +27,22 @@ class resnet50(nn.Module):
             param.requires_grad = False
         for param in self.model.fc.parameters():
             param.requires_grad = True
-    
+
     def unfreeze(self):
         for param in self.model.parameters():
             param.requires_grad = True
+
+
+#ResNet50:
+class resnet50(BaseDetector):
+
+    def __init__(self):
+        super(resnet50, self).__init__()
+        self.model = timm.create_model('resnet50', pretrained=True)
+        self.model.fc = nn.Linear(self.model.fc.in_features, 1)
+        torch.nn.init.normal_(self.model.fc.weight.data, 0.0, .02)
+        torch.nn.init.zeros_(self.model.fc.bias.data)
+
 
 #ResNet101:
 class resnet101(nn.Module):
@@ -38,69 +51,29 @@ class resnet101(nn.Module):
         super(resnet101, self).__init__()
         self.model = timm.create_model('resnet101', pretrained=True)
         self.model.fc = nn.Linear(self.model.fc.in_features, 1)
-        self.sigmoid = nn.Sigmoid()
-
-    def forward(self, x):
-        x = self.model(x)
-        return x
-
-    def freeze(self):
-        for param in self.model.parameters():
-            param.requires_grad = False
-        for param in self.model.fc.parameters():
-            param.requires_grad = True
-    
-    def unfreeze(self):
-        for param in self.model.parameters():
-            param.requires_grad = True
+        torch.nn.init.normal_(self.model.fc.weight.data, 0.0, .02)
+        torch.nn.init.zeros_(self.model.fc.bias.data)
 
 #ViT-large
-
 class vit_large(nn.Module):
 
     def __init__(self):
         super(vit_large, self).__init__()
         self.model = timm.create_model('vit_large_patch16_224', pretrained=True)
         self.model.head = nn.Linear(self.model.head.in_features, 1)
-        self.sigmoid = nn.Sigmoid()
-    
-    def forward(self, x):
-        x = self.model(x)
-        return x
+        trunc_normal_(self.model.head.weight.data, std=.02)
+        torch.nn.init.zeros_(self.model.head.bias.data)
 
-    def freeze(self):
-        for param in self.model.parameters():
-            param.requires_grad = False
-        for param in self.model.head.parameters():
-            param.requires_grad = True
-    
-    def unfreeze(self):
-        for param in self.model.parameters():
-            param.requires_grad = True
 
 #ViT-base
-
 class vit_base(nn.Module):
 
     def __init__(self):
         super(vit_base, self).__init__()
         self.model = timm.create_model('vit_base_patch16_224', pretrained=True)
         self.model.head = nn.Linear(self.model.head.in_features, 1)
-        self.sigmoid = nn.Sigmoid()
-    
-    def forward(self, x):
-        x = self.model(x)
-        return x
-
-    def freeze(self):
-        for param in self.model.parameters():
-            param.requires_grad = False
-        for param in self.model.head.parameters():
-            param.requires_grad = True
-    
-    def unfreeze(self):
-        for param in self.model.parameters():
-            param.requires_grad = True
+        trunc_normal_(self.model.head.weight.data, std=.02)
+        torch.nn.init.zeros_(self.model.head.bias.data)
 
 
 #vit-small
@@ -110,21 +83,8 @@ class vit_small(nn.Module):
         super(vit_small, self).__init__()
         self.model = timm.create_model('vit_small_patch16_224', pretrained=True)
         self.model.head = nn.Linear(self.model.head.in_features, 1)
-        self.sigmoid = nn.Sigmoid()
-    
-    def forward(self, x):
-        x = self.model(x)
-        return x
-
-    def freeze(self):
-        for param in self.model.parameters():
-            param.requires_grad = False
-        for param in self.model.head.parameters():
-            param.requires_grad = True
-    
-    def unfreeze(self):
-        for param in self.model.parameters():
-            param.requires_grad = True
+        trunc_normal_(self.model.head.weight.data, std=.02)
+        torch.nn.init.zeros_(self.model.head.bias.data)
 
 
 #vit-tiny
@@ -134,45 +94,19 @@ class vit_tiny(nn.Module):
         super(vit_tiny, self).__init__()
         self.model = timm.create_model('vit_tiny_patch16_224', pretrained=True)
         self.model.head = nn.Linear(self.model.head.in_features, 1)
-        self.sigmoid = nn.Sigmoid()
-    
-    def forward(self, x):
-        x = self.model(x)
-        return x
+        trunc_normal_(self.model.head.weight.data, std=.02)
+        torch.nn.init.zeros_(self.model.head.bias.data)
 
-    def freeze(self):
-        for param in self.model.parameters():
-            param.requires_grad = False
-        for param in self.model.head.parameters():
-            param.requires_grad = True
-    
-    def unfreeze(self):
-        for param in self.model.parameters():
-            param.requires_grad = True
 
 #Swin-base-Transformer
-
 class swin_small(nn.Module):
 
     def __init__(self):
         super(swin_small, self).__init__()
         self.model = timm.create_model('swin_small_patch4_window7_224', pretrained=True)
         self.model.head = nn.Linear(self.model.head.in_features, 1)
-        self.sigmoid = nn.Sigmoid()
-
-    def forward(self, x):
-        x = self.model(x)
-        return x
-
-    def freeze(self):
-        for param in self.model.parameters():
-            param.requires_grad = False
-        for param in self.model.head.parameters():
-            param.requires_grad = True
-    
-    def unfreeze(self):
-        for param in self.model.parameters():
-            param.requires_grad = True
+        trunc_normal_(self.model.head.weight.data, std=.02)
+        torch.nn.init.zeros_(self.model.head.bias.data)
 
 
 # swin tiny
@@ -182,67 +116,27 @@ class swin_tiny(nn.Module):
         super(swin_tiny, self).__init__()
         self.model = timm.create_model('swin_tiny_patch4_window7_224', pretrained=True)
         self.model.head = nn.Linear(self.model.head.in_features, 1)
-        self.sigmoid = nn.Sigmoid()
-
-    def forward(self, x):
-        x = self.model(x)
-        return x
-
-    def freeze(self):
-        for param in self.model.parameters():
-            param.requires_grad = False
-        for param in self.model.head.parameters():
-            param.requires_grad = True
-    
-    def unfreeze(self):
-        for param in self.model.parameters():
-            param.requires_grad = True
+        trunc_normal_(self.model.head.weight.data, std=.02)
+        torch.nn.init.zeros_(self.model.head.bias.data)
 
 
 # inception
-
 class inception_v4(nn.Module):
 
     def __init__(self):
         super(inception_v4, self).__init__()
         self.model = timm.create_model('inception_v4', pretrained=True)
         self.model.last_linear = nn.Linear(self.model.last_linear.in_features, 1)
-        self.sigmoid = nn.Sigmoid()
+        torch.nn.init.normal_(self.model.last_linear.weight.data, 0.0, .02)
+        torch.nn.init.zeros_(self.model.last_linear.bias.data)
 
-    def forward(self, x):
-        x = self.model(x)
-        return x
-
-    def freeze(self):
-        for param in self.model.parameters():
-            param.requires_grad = False
-        for param in self.model.fc.parameters():
-            param.requires_grad = True
-    
-    def unfreeze(self):
-        for param in self.model.parameters():
-            param.requires_grad = True
 
 # xception
-
 class xception(nn.Module):
 
     def __init__(self):
         super(xception, self).__init__()
         self.model = timm.create_model('xception', pretrained=True)
         self.model.fc = nn.Linear(self.model.fc.in_features, 1)
-        self.sigmoid = nn.Sigmoid()
-
-    def forward(self, x):
-        x = self.model(x)
-        return x
-
-    def freeze(self):
-        for param in self.model.parameters():
-            param.requires_grad = False
-        for param in self.model.fc.parameters():
-            param.requires_grad = True
-    
-    def unfreeze(self):
-        for param in self.model.parameters():
-            param.requires_grad = True
+        torch.nn.init.normal_(self.model.fc.weight.data, 0.0, .02)
+        torch.nn.init.zeros_(self.model.fc.bias.data)
