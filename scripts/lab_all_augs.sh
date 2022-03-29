7db92a29872ad5bc0a7403ec9965c59cfd879d97
Giorgos Kordopatis-Zilos <georgekordopatis@iti.gr>
2022-03-29 11:40:38 +0300

    Add geometric augmentations

--- scripts/lab_all_augs.sh
+++ scripts/lab_all_augs.sh
@@ -4,3 +4,8 @@
 ./scripts/lab_train.sh $1 oneof $2 $3
 ./scripts/lab_train.sh $1 strong $2 $3
 ./scripts/lab_train.sh $1 soft $2 $3
+
+./scripts/lab_train.sh $1 Wang-geometric $2 $3
+./scripts/lab_train.sh $1 oneof-geometric $2 $3
+./scripts/lab_train.sh $1 strong-geometric $2 $3
+./scripts/lab_train.sh $1 soft-geometric $2 $3
